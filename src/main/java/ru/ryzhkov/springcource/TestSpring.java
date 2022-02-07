fe0aaddea6426d6dcf8de3a7a433af92f940901d
yuri018 <plktmn@gmail.com>
2022-02-07 19:13:27 +0500

    Manuelles Hinzufügen von Werten, über Setter

--- src/main/java/ru/ryzhkov/springcource/TestSpring.java
+++ src/main/java/ru/ryzhkov/springcource/TestSpring.java
@@ -8,9 +8,14 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        Music music = context.getBean("musicBean", Music.class);
+//        // Dependency injection (music) в объект MusicPlayer вручную
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
